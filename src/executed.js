bb7ae4f7673185024a953f232fbe98ba799737ec
Nonudeveloper <exp.developer001@gmail.com>
2018-08-02 15:50:57 +0530

    source code

---
+++ src/executed.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import Collapse from 'material-ui/transitions/Collapse';
+import IconButton from 'material-ui/IconButton';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+
+import ExpandLessIcon from 'material-ui-icons/ExpandLess';
+import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
+
+import NotebookPreview from '@nteract/notebook-preview';
+
+class JupyterProblem extends React.PureComponent {
+  state = {
+    collapses: {
+      provided: false,
+      problem: true,
+    },
+    problemJson: null,
+  };
+
+  loadProblem = () => {
+    this.setState({ problemJson: <NotebookPreview notebook={window.problem} /> });
+  };
+
+  render() {
+    return (
+      <Paper style={{ margin: '24px 2px' }}>
+        <Typography style={{ position: 'relative' }} variant="headline">
+          <span>Problem</span>
+          <IconButton
+            onClick={() => this.onSwitchCollapse('problem')}
+            style={{
+              position: 'absolute',
+              right: 0,
+            }}
+          >
+            {this.state.collapses.problem ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+          </IconButton>
+        </Typography>
+        <Collapse collapsedHeight="10px" in={this.state.collapses.problem}>
+          <div
+            style={{
+              textAlign: 'left',
+            }}
+          >
+            <div id="NotebookPreview">{this.state.problemJson}</div>
+            <button style={{ visibility: 'hidden' }} onClick={this.loadProblem}>
+              Load Problem
+            </button>
+          </div>
+        </Collapse>
+      </Paper>
+    );
+  }
+}
+
+export default JupyterProblem;
